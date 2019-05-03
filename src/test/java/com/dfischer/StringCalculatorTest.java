21cb223c2bb8ccfbd6e5310ef7506e820cde6035
Doug Fischer <douglas.fischer@philips.com>
2019-05-03 09:27:54 -0400

    Duplication cleanup

--- src/test/java/com/dfischer/StringCalculatorTest.java
+++ src/test/java/com/dfischer/StringCalculatorTest.java
@@ -1,21 +1,26 @@
 package com.dfischer;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
+    private StringCalculator calculator;
+
+    @Before
+    public void setup() {
+        this.calculator = new StringCalculator();
+    }
 
     @Test
     public void singleNumberStringReturnsNumber() {
-        StringCalculator calculator = new StringCalculator();
         assertEquals(1, calculator.add("1"));
         assertEquals(42, calculator.add("42"));
     }
 
     @Test
     public void twoCommaDelimitedNumbersInStringReturnsSum() {
-        StringCalculator calculator = new StringCalculator();
         assertEquals(2, calculator.add("1,1"));
         assertEquals(5, calculator.add("2,3"));
     }
